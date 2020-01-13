8119e60f032bb1d0ec5ba404145850efb79238da
kurtturner21 <kurtturner21@gmail.com>
2020-01-12 18:09:36 -0600

    Create hello_world.py

---
+++ a2/hello_world.py
@@ -0,0 +1,18 @@
+"""
+Author:  Kurt Turner
+Date: 01/12/2010
+Goal: Hello world statement.
+"""
+
+# declare some vars
+my_name = 'Kurt'
+age = 18
+state = 'NY'
+height = 42.95
+
+# use my vars in print statements
+print('Hello world')
+print(' - my name is ' + my_name)
+print(' - my age is ' + str(age))
+print(' - I live in {0}.'.format(state))
+print(' - I am ' + str(height) + ' tall')
